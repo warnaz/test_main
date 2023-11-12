64507ea9f4167ad1782ee2de6a2300132756a407
Magomed <negotiumnomen@gmail.com>
2023-11-12 17:43:23 +0300

    hi

--- config_models.py
+++ config_models.py
@@ -2,6 +2,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+host, port = "dpg-cl8e4776e7vc73a56bpg-a", 5432
+login, password = "admin", 'tO7TsKPwarM40UVmoDAMcQkSJpzcXQpG'
+db_name = "postgres_test_jcu8"
 
-login, password = "postgres", "master"
-engine = create_engine(url=f"postgresql://{login}:{password}@db/postgres", echo=True)
+engine = create_engine(url=f"postgresql://{login}:{password}@{host}/{db_name}", echo=True)
